1cd94138178c8437e5dbdb59bbf1e2737663c639
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-01-09 14:54:49 +0600

    React installed and checking for github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Welcome to react mini app ready</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
