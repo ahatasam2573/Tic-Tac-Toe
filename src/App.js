01b769b01906995ff868fab37966c64090f0e545
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-01-09 17:33:00 +0600

    main ui created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-
+import Square from "./components/Square";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Welcome to react mini app ready</h3>
-      </header>
+    <div className="container">
+      <div className="left-wrapper">
+        <div className="left-text">
+          Let's play the game
+        </div>
+        <div className="left-button">Start a New Game</div>
+      </div>
+      <div className="right-wrapper">
+        <div className="players">
+          <div className="player first-player">
+            Player X
+          </div>
+          <div className="player second-player">
+            Player O
+          </div>
+        </div>
+        <div className="games-ground">
+          <Square id={0} className='border-RB' />
+          <Square id={1} className='border-RB' />
+          <Square id={2} className='border-B' />
+          <Square id={3} className='border-RB' />
+          <Square id={4} className='border-RB' />
+          <Square id={5} className='border-B' />
+          <Square id={6} className='border-R' />
+          <Square id={7} className='border-R' />
+          <Square id={8} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Square.css';
+
+const Square = () => {
+    return (
+        <div className='square-container'>
+            X
+        </div>
+    );
+};
+
+export default Square;
