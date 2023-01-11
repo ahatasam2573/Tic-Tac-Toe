72bab9d2f67a14a9b0e316ff9b6218842bca84ef
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2023-01-11 16:13:22 +0600

    initial ground setup completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import Square from "./components/Square";
 import './App.css';
+import { useState } from "react";
+
+const initialGameGround = ["", "", "", "", "", "", "", "", ""];
 
 function App() {
+
+  const [groundState, setGroundState] = useState(initialGameGround);
+  const [steps, setSteps] = useState(0);
+  const onClickHandler = event => {
+    const copyGameGround = [...groundState]
+    if (!event.target.innerText) {
+      copyGameGround[event.target.id] = steps % 2 === 0 ? 'X' : 'O';
+      setSteps(steps + 1);
+      setGroundState(copyGameGround);
+    }
+  }
   return (
     <div className="container">
       <div className="left-wrapper">
@@ -19,16 +33,16 @@ function App() {
             Player O
           </div>
         </div>
-        <div className="games-ground">
-          <Square id={0} className='border-RB' />
-          <Square id={1} className='border-RB' />
-          <Square id={2} className='border-B' />
-          <Square id={3} className='border-RB' />
-          <Square id={4} className='border-RB' />
-          <Square id={5} className='border-B' />
-          <Square id={6} className='border-R' />
-          <Square id={7} className='border-R' />
-          <Square id={8} />
+        <div className="games-ground" onClick={onClickHandler}>
+          <Square id={0} className='border-RB' state={groundState[0]} />
+          <Square id={1} className='border-RB' state={groundState[1]} />
+          <Square id={2} className='border-B' state={groundState[2]} />
+          <Square id={3} className='border-RB' state={groundState[3]} />
+          <Square id={4} className='border-RB' state={groundState[4]} />
+          <Square id={5} className='border-B' state={groundState[5]} />
+          <Square id={6} className='border-R' state={groundState[6]} />
+          <Square id={7} className='border-R' state={groundState[7]} />
+          <Square id={8} state={groundState[8]} />
         </div>
       </div>
     </div>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Square.css';
 
-const Square = () => {
+const Square = ({ id, className, state }) => {
     return (
-        <div className='square-container'>
-            X
-        </div>
+        <div className={`square-container ${className}`} id={id} >
+            {state}
+        </div >
     );
 };
 
